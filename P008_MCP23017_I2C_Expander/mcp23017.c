4c726ffc67b97869c3364e53227dd3a87127e578
Marcel@Home <geekmarcel@outlook.com>
2015-10-28 19:58:23 +0100

    Added defines for MCP23017 and started with wrapper functions for MCP23017.

--- P008_MCP23017_I2C_Expander/mcp23017.c
+++ P008_MCP23017_I2C_Expander/mcp23017.c
@@ -15,13 +15,13 @@
  *--------------------------------------------------------------------------------------------------------------------------------------------------------*/
 
 /************************************************************************/
-/* Defines				                                                */
+/* Defines													   */
 /************************************************************************/
 #define F_CPU			16000000UL
 
 
 /************************************************************************/
-/* Includes				                                                */
+/* Includes													   */
 /************************************************************************/
 #include <avr/io.h>
 #include "util/delay.h"
@@ -30,11 +30,513 @@
 
 
 /************************************************************************/
-/* Structures				                                            */
+/* Structures													   */
 /************************************************************************/
-
+struct MCP23017
+{
+	BYTE address;
+	BankInUse bank;
+	
+}mcp23017;
 
 
 /************************************************************************/
-/* Functions				                                            */
+/* Functions													   */
 /************************************************************************/	
+
+
+/***************************************************************************
+*  Function:		SetPortDirection(MCP23017_Port port, BYTE value)
+*  Description:		Sets the direction port register. When a bit is set the corresponding
+*				pin becomes an input and when its cleared the pin becomes an output.
+*  Receives:		MCP23017_Port port		:	The port on the MCP23017 (MCP23017_PORTA or MCP23017_PORTB).
+*				BYTE value			:	The value to set.
+*  Returns:		Nothing
+***************************************************************************/
+void SetPortDirection(MCP23017_Port port, BYTE value)
+{
+	if(BankInUse == BANK0)
+	{
+		if(port == MCP23017_PORTA)
+		{
+			i2c_send(address, MCP23017_IODIRA, value);
+		}
+		else if(port == MCP23017_PORTB)
+		{
+			i2c_send(address, MCP23017_IODIRB, value);
+		}
+	}
+	else if(BankInUse == BANK1)
+	{
+		if(port == MCP23017_PORTA)
+		{
+			i2c_send(address, MCP23017_IODIRA_BANK1, value);
+		}
+		else if(port == MCP23017_PORTB)
+		{
+			i2c_send(address, MCP23017_IODIRB_BANK1, value);
+		}
+	}
+}
+
+/***************************************************************************
+*  Function:		BYTE ReadPortDirection(MCP23017_Port port)
+*				Bits which are set are inputs, and bit that are cleared are outputs.
+*  Description:		Reads the direction port register.
+*  Receives:		MCP23017_Port port		:	The port on the MCP23017 (MCP23017_PORTA or MCP23017_PORTB).
+*  Returns:		Byte that was read.
+***************************************************************************/
+BYTE ReadPortDirection(MCP23017_Port port)
+{
+	BYTE byteRead = 0;
+	
+	if(BankInUse == BANK0)
+	{
+		if(port == MCP23017_PORTA)
+		{
+			byteRead = i2c_read1byte(address, MCP23017_IODIRA);
+		}
+		else if(port == MCP23017_PORTB)
+		{
+			byteRead = i2c_read1byte(address, MCP23017_IODIRB);
+		}
+	}
+	else if(BankInUse == BANK1)
+	{
+		if(port == MCP23017_PORTA)
+		{
+			byteRead = i2c_read1byte(address, MCP23017_IODIRA_BANK1);
+		}
+		else if(port == MCP23017_PORTB)
+		{
+			byteRead = i2c_read1byte(address, MCP23017_IODIRB_BANK1);
+		}
+	}
+	
+	return byteRead;
+}
+
+/***************************************************************************
+*  Function:		SetPortPolarity(MCP23017_Port port, BYTE value)
+*  Description:		Sets the port polarity register, when the corresponding bit is set
+*				the GPIO register bit will reflect the inverted value of the pin.
+*  Receives:		MCP23017_Port port		:	The port on the MCP23017 (MCP23017_PORTA or MCP23017_PORTB).
+*				BYTE value				:	The value to set.
+*  Returns:		Nothing
+***************************************************************************/
+void SetPortPolarity(MCP23017_Port port, BYTE value)
+{
+	if(BankInUse == BANK0)
+	{
+		if(port == MCP23017_PORTA)
+		{
+			i2c_send(address, MCP23017_IPOLA, value);
+		}
+		else if(port == MCP23017_PORTB)
+		{
+			i2c_send(address, MCP23017_IPOLB, value);
+		}
+	}
+	else if(BankInUse == BANK1)
+	{
+		if(port == MCP23017_PORTA)
+		{
+			i2c_send(address, MCP23017_IPOLA_BANK1, value);
+		}
+		else if(port == MCP23017_PORTB)
+		{
+			i2c_send(address, MCP23017_IPOLB_BANK1, value);
+		}
+	}
+}
+
+/***************************************************************************
+*  Function:		BYTE ReadPortDirection(MCP23017_Port port)
+*  Description:		Reads the input polarity register.
+*  Receives:		MCP23017_Port port		:	The port on the MCP23017 (MCP23017_PORTA or MCP23017_PORTB).
+*  Returns:		Byte that was read.
+***************************************************************************/
+BYTE ReadPortDirection(MCP23017_Port port)
+{
+	BYTE byteRead = 0;
+	
+	if(BankInUse == BANK0)
+	{
+		if(port == MCP23017_PORTA)
+		{
+			byteRead = i2c_read1byte(address, MCP23017_IPOLA);
+		}
+		else if(port == MCP23017_PORTB)
+		{
+			byteRead = i2c_read1byte(address, MCP23017_IPOLB);
+		}
+	}
+	else if(BankInUse == BANK1)
+	{
+		if(port == MCP23017_PORTA)
+		{
+			byteRead = i2c_read1byte(address, MCP23017_IPOLA_BANK1);
+		}
+		else if(port == MCP23017_PORTB)
+		{
+			byteRead = i2c_read1byte(address, MCP23017_IPOLB_BANK1);
+		}
+	}
+	
+	return byteRead;
+}
+
+/***************************************************************************
+*  Function:		SetIntOnChange(MCP23017_Port port, BYTE value)
+*  Description:		Sets the interrupt-on-change register, if a bit is set the
+*				corresponding pin is enabled for interrupt-on-change.
+*  Receives:		MCP23017_Port port		:	The port on the MCP23017 (MCP23017_PORTA or MCP23017_PORTB).
+*				BYTE value				:	The value to set.
+*  Returns:		Nothing
+***************************************************************************/
+void SetIntOnChange(MCP23017_Port port, BYTE value)
+{
+	if(BankInUse == BANK0)
+	{
+		if(port == MCP23017_PORTA)
+		{
+			i2c_send(address, MCP23017_GPINTENA, value);
+		}
+		else if(port == MCP23017_PORTB)
+		{
+			i2c_send(address, MCP23017_GPINTENB, value);
+		}
+	}
+	else if(BankInUse == BANK1)
+	{
+		if(port == MCP23017_PORTA)
+		{
+			i2c_send(address, MCP23017_GPINTENA_BANK1, value);
+		}
+		else if(port == MCP23017_PORTB)
+		{
+			i2c_send(address, MCP23017_GPINTENB_BANK1, value);
+		}
+	}
+}
+
+/***************************************************************************
+*  Function:		BYTE ReadIntOnChange(MCP23017_Port port)
+*  Description:		Reads the interrupt-on-change register.
+*  Receives:		MCP23017_Port port		:	The port on the MCP23017 (MCP23017_PORTA or MCP23017_PORTB).
+*  Returns:		Byte that was read.
+***************************************************************************/
+BYTE ReadIntOnChange(MCP23017_Port port)
+{
+	BYTE byteRead = 0;
+	
+	if(BankInUse == BANK0)
+	{
+		if(port == MCP23017_PORTA)
+		{
+			byteRead = i2c_read1byte(address, MCP23017_GPINTENA);
+		}
+		else if(port == MCP23017_PORTB)
+		{
+			byteRead = i2c_read1byte(address, MCP23017_GPINTENB);
+		}
+	}
+	else if(BankInUse == BANK1)
+	{
+		if(port == MCP23017_PORTA)
+		{
+			byteRead = i2c_read1byte(address, MCP23017_GPINTENA_BANK1);
+		}
+		else if(port == MCP23017_PORTB)
+		{
+			byteRead = i2c_read1byte(address, MCP23017_GPINTENB_BANK1);
+		}
+	}
+	
+	return byteRead;
+}
+
+/***************************************************************************
+*  Function:		SetDefaultCompareReg(MCP23017_Port port, BYTE value)
+*  Description:		Sets the default compare register, these bits set the compare value
+*				for pins configured for interrupt-on-change, if the associated pin
+*				level is the opposite from the register bit, an interrupt occurs.
+*  Receives:		MCP23017_Port port		:	The port on the MCP23017 (MCP23017_PORTA or MCP23017_PORTB).
+*				BYTE value				:	The value to set.
+*  Returns:		Nothing
+***************************************************************************/
+void SetDefaultCompareReg(MCP23017_Port port, BYTE value)
+{
+	if(BankInUse == BANK0)
+	{
+		if(port == MCP23017_PORTA)
+		{
+			i2c_send(address, MCP23017_DEFVALA, value);
+		}
+		else if(port == MCP23017_PORTB)
+		{
+			i2c_send(address, MCP23017_DEFVALB, value);
+		}
+	}
+	else if(BankInUse == BANK1)
+	{
+		if(port == MCP23017_PORTA)
+		{
+			i2c_send(address, MCP23017_DEFVALA_BANK1, value);
+		}
+		else if(port == MCP23017_PORTB)
+		{
+			i2c_send(address, MCP23017_DEFVALB_BANK1, value);
+		}
+	}
+}
+
+/***************************************************************************
+*  Function:		BYTE ReadDefaultCompareReg(MCP23017_Port port)
+*  Description:		Reads the default compare register for interrupt-on-change.
+*  Receives:		MCP23017_Port port		:	The port on the MCP23017 (MCP23017_PORTA or MCP23017_PORTB).
+*  Returns:		Byte that was read.
+***************************************************************************/
+BYTE ReadDefaultCompareReg(MCP23017_Port port)
+{
+	BYTE byteRead = 0;
+	
+	if(BankInUse == BANK0)
+	{
+		if(port == MCP23017_PORTA)
+		{
+			byteRead = i2c_read1byte(address, MCP23017_DEFVALA);
+		}
+		else if(port == MCP23017_PORTB)
+		{
+			byteRead = i2c_read1byte(address, MCP23017_DEFVALB);
+		}
+	}
+	else if(BankInUse == BANK1)
+	{
+		if(port == MCP23017_PORTA)
+		{
+			byteRead = i2c_read1byte(address, MCP23017_DEFVALA_BANK1);
+		}
+		else if(port == MCP23017_PORTB)
+		{
+			byteRead = i2c_read1byte(address, MCP23017_DEFVALB_BANK1);
+		}
+	}
+	
+	return byteRead;
+}
+
+/***************************************************************************
+*  Function:		SetIntControlReg(MCP23017_Port port, BYTE value)
+*  Description:		Sets the interrupt control register, this register controls how the associated
+*				pin value is compared for the interrupt-on-change.
+*				If a bit is set, the corresponding IO pin is compared against the
+*				associated bit in the DEFVAL register.
+*				If a bit value is clear, the corresponding IO pin is compared against the previous value.
+*  Receives:		MCP23017_Port port		:	The port on the MCP23017 (MCP23017_PORTA or MCP23017_PORTB).
+*				BYTE value				:	The value to set.
+*  Returns:		Nothing
+***************************************************************************/
+void SetIntControlReg(MCP23017_Port port, BYTE value)
+{
+	if(BankInUse == BANK0)
+	{
+		if(port == MCP23017_PORTA)
+		{
+			i2c_send(address, MCP23017_INTCONA, value);
+		}
+		else if(port == MCP23017_PORTB)
+		{
+			i2c_send(address, MCP23017_INTCONB, value);
+		}
+	}
+	else if(BankInUse == BANK1)
+	{
+		if(port == MCP23017_PORTA)
+		{
+			i2c_send(address, MCP23017_INTCONA_BANK1, value);
+		}
+		else if(port == MCP23017_PORTB)
+		{
+			i2c_send(address, MCP23017_INTCONB_BANK1, value);
+		}
+	}
+}
+
+/***************************************************************************
+*  Function:		BYTE ReadIntControlReg(MCP23017_Port port)
+*  Description:		Reads the interrupt control register.
+*  Receives:		MCP23017_Port port		:	The port on the MCP23017 (MCP23017_PORTA or MCP23017_PORTB).
+*  Returns:		Byte that was read.
+***************************************************************************/
+BYTE ReadIntControlReg(MCP23017_Port port)
+{
+	BYTE byteRead = 0;
+	
+	if(BankInUse == BANK0)
+	{
+		if(port == MCP23017_PORTA)
+		{
+			byteRead = i2c_read1byte(address, MCP23017_INTCONA);
+		}
+		else if(port == MCP23017_PORTB)
+		{
+			byteRead = i2c_read1byte(address, MCP23017_INTCONB);
+		}
+	}
+	else if(BankInUse == BANK1)
+	{
+		if(port == MCP23017_PORTA)
+		{
+			byteRead = i2c_read1byte(address, MCP23017_INTCONA_BANK1);
+		}
+		else if(port == MCP23017_PORTB)
+		{
+			byteRead = i2c_read1byte(address, MCP23017_INTCONB_BANK1);
+		}
+	}
+	
+	return byteRead;
+}
+
+/***************************************************************************
+*  Function:		SetIoConfigReg(MCP23017_Port port, BYTE value)
+*  Description:		Sets the IO Expander Configuration register, this register contains settings
+*				that determine how the IO Expander behaves.
+*  Receives:		MCP23017_Port port		:	The port on the MCP23017 (MCP23017_PORTA or MCP23017_PORTB).
+*				BYTE value				:	The value to set.
+*  Returns:		Nothing
+***************************************************************************/
+void SetIoConfigReg(MCP23017_Port port, BYTE value)
+{
+	if(BankInUse == BANK0)
+	{
+		if(port == MCP23017_PORTA)
+		{
+			i2c_send(address, MCP23017_IOCONA, value);
+		}
+		else if(port == MCP23017_PORTB)
+		{
+			i2c_send(address, MCP23017_IOCONB, value);
+		}
+	}
+	else if(BankInUse == BANK1)
+	{
+		if(port == MCP23017_PORTA)
+		{
+			i2c_send(address, MCP23017_IOCONA_BANK1, value);
+		}
+		else if(port == MCP23017_PORTB)
+		{
+			i2c_send(address, MCP23017_IOCONB_BANK1, value);
+		}
+	}
+}
+
+/***************************************************************************
+*  Function:		BYTE ReadIntControlReg(MCP23017_Port port)
+*  Description:		Reads the IO Expander Configuration register.
+*  Receives:		MCP23017_Port port		:	The port on the MCP23017 (MCP23017_PORTA or MCP23017_PORTB).
+*  Returns:		Byte that was read.
+***************************************************************************/
+BYTE ReadIoConfigReg(MCP23017_Port port)
+{
+	BYTE byteRead = 0;
+	
+	if(BankInUse == BANK0)
+	{
+		if(port == MCP23017_PORTA)
+		{
+			byteRead = i2c_read1byte(address, MCP23017_IOCONA);
+		}
+		else if(port == MCP23017_PORTB)
+		{
+			byteRead = i2c_read1byte(address, MCP23017_IOCONB);
+		}
+	}
+	else if(BankInUse == BANK1)
+	{
+		if(port == MCP23017_PORTA)
+		{
+			byteRead = i2c_read1byte(address, MCP23017_IOCONA_BANK1);
+		}
+		else if(port == MCP23017_PORTB)
+		{
+			byteRead = i2c_read1byte(address, MCP23017_IOCONB_BANK1);
+		}
+	}
+	
+	return byteRead;
+}
+
+/***************************************************************************
+*  Function:		SetPullupConfigReg(MCP23017_Port port, BYTE value)
+*  Description:		Sets the pull-up resistor configuration register, if a bit is set
+*				and the corresponding pin is configured as an input the corresponding
+*				port pin is internally pulled up with a 100 kOhm resistor.
+*  Receives:		MCP23017_Port port		:	The port on the MCP23017 (MCP23017_PORTA or MCP23017_PORTB).
+*				BYTE value				:	The value to set.
+*  Returns:		Nothing
+***************************************************************************/
+void SetPullupConfigReg(MCP23017_Port port, BYTE value)
+{
+	if(BankInUse == BANK0)
+	{
+		if(port == MCP23017_PORTA)
+		{
+			i2c_send(address, MCP23017_GPPUA, value);
+		}
+		else if(port == MCP23017_PORTB)
+		{
+			i2c_send(address, MCP23017_GPPUB, value);
+		}
+	}
+	else if(BankInUse == BANK1)
+	{
+		if(port == MCP23017_PORTA)
+		{
+			i2c_send(address, MCP23017_GPPUA_BANK1, value);
+		}
+		else if(port == MCP23017_PORTB)
+		{
+			i2c_send(address, MCP23017_GPPUB_BANK1, value);
+		}
+	}
+}
+
+/***************************************************************************
+*  Function:		BYTE ReadPullupConfigReg(MCP23017_Port port)
+*  Description:		Reads pull-up configuration register.
+*  Receives:		MCP23017_Port port		:	The port on the MCP23017 (MCP23017_PORTA or MCP23017_PORTB).
+*  Returns:		Byte that was read.
+***************************************************************************/
+BYTE ReadPullupConfigReg(MCP23017_Port port)
+{
+	BYTE byteRead = 0;
+	
+	if(BankInUse == BANK0)
+	{
+		if(port == MCP23017_PORTA)
+		{
+			byteRead = i2c_read1byte(address, MCP23017_GPPUA);
+		}
+		else if(port == MCP23017_PORTB)
+		{
+			byteRead = i2c_read1byte(address, MCP23017_GPPUB);
+		}
+	}
+	else if(BankInUse == BANK1)
+	{
+		if(port == MCP23017_PORTA)
+		{
+			byteRead = i2c_read1byte(address, MCP23017_GPPUA_BANK1);
+		}
+		else if(port == MCP23017_PORTB)
+		{
+			byteRead = i2c_read1byte(address, MCP23017_GPPUB_BANK1);
+		}
+	}
+	
+	return byteRead;
+}
--- P008_MCP23017_I2C_Expander/mcp23017.h
+++ P008_MCP23017_I2C_Expander/mcp23017.h
@@ -20,10 +20,118 @@
 
 
 #include "common.h"
+/************************************************************************/
+/* Enumerations												   */
+/************************************************************************/
 
+enum{BANK0, BANK1} BankInUse;
+enum{MCP23017_PORTA, MCP23017_PORTB} MCP23017_Port;
+	
+	
 /************************************************************************/
-/* Defines				                                                */
+/* Defines													   */
 /************************************************************************/
+
+enum{BANK0, BANK1} BankInUse;
+enum{MCP23017_PORTA, MCP23017_PORTB} MCP23017_Port;
+
+/* Address pins */
+#define MCP23017_ADDR_PIN0          0x01     /*A0*/
+#define MCP23017_ADDR_PIN1          0x02     /*A1*/
+#define MCP23017_ADDR_PIN2          0x04     /*A2*/
+
+/* 7 Bit Addresses */
+#define MCP23017_ADDRESS            0x20
+#define MCP23017_ADDRESS_0          0x20
+#define MCP23017_ADDRESS_1          0x21
+#define MCP23017_ADDRESS_2          0x22
+#define MCP23017_ADDRESS_3          0x23
+#define MCP23017_ADDRESS_4          0x24
+#define MCP23017_ADDRESS_5          0x25
+#define MCP23017_ADDRESS_6          0x26
+#define MCP23017_ADDRESS_7          0x27
+
+/* 8 Bit Addresses */
+#define MCP23017_8B_ADDRESS         0x40
+#define MCP23017_8B_ADDRESS_0       0x40
+#define MCP23017_8B_ADDRESS_1       0x42
+#define MCP23017_8B_ADDRESS_2       0x44
+#define MCP23017_8B_ADDRESS_3       0x46
+#define MCP23017_8B_ADDRESS_4       0x48
+#define MCP23017_8B_ADDRESS_5       0x4A
+#define MCP23017_8B_ADDRESS_6       0x4C
+#define MCP23017_8B_ADDRESS_7       0x4E
+
+/* Register Addresses if BANK = 0 */
+#define MCP23017_IODIRA             0x00    /*I/O DIRECTION REGISTER A*/
+#define MCP23017_IODIRB             0x01    /*I/O DIRECTION REGISTER B*/
+#define MCP23017_IPOLA              0x02    /*INPUT POLARITY REGISTER A*/
+#define MCP23017_IPOLB              0x03    /*INPUT POLARITY REGISTER B*/
+#define MCP23017_GPINTENA           0x04    /*INTERRUPT-ON-CHANGE CONTROL REGISTER A*/
+#define MCP23017_GPINTENB           0x05    /*INTERRUPT-ON-CHANGE CONTROL REGISTER B*/
+#define MCP23017_DEFVALA            0x06    /*DEFAULT COMPARE REGISTER FOR INTERRUPT-ON-CHANGE A*/
+#define MCP23017_DEFVALB            0x07    /*DEFAULT COMPARE REGISTER FOR INTERRUPT-ON-CHANGE B*/
+#define MCP23017_INTCONA            0x08    /*INTERRUPT CONTROL REGISTER A*/
+#define MCP23017_INTCONB            0x09    /*INTERRUPT CONTROL REGISTER B*/
+#define MCP23017_IOCONA             0x0A    /*CONFIGURATION REGISTER A*/
+#define MCP23017_IOCONB             0x0B    /*CONFIGURATION REGISTER B*/
+#define MCP23017_GPPUA              0x0C    /*PULL-UP RESISTOR A*/
+#define MCP23017_GPPUB              0x0D    /*PULL-UP RESISTOR B*/
+#define MCP23017_INTFA              0x0E    /*INTERRUPT FLAG REGISTER A*/
+#define MCP23017_INTFB              0x0F    /*INTERRUPT FLAG REGISTER B*/
+#define MCP23017_INTCAPA            0x10    /*INTERRUPT CAPTURE REGISTER A*/
+#define MCP23017_INTCAPB            0x11    /*INTERRUPT CAPTURE REGISTER B*/
+#define MCP23017_GPIOA              0x12    /*PORT REGISTER A*/
+#define MCP23017_GPIOB              0x13    /*PORT REGISTER B*/
+#define MCP23017_OLATA              0x14    /*OUTPUT LATCH REGISTER  A*/
+#define MCP23017_OLATB              0x15    /*OUTPUT LATCH REGISTER  B*/
+
+#define MCP23017_IOCON              MCP23017_IOCONA
+
+/*Register addresses if BANK = 1 */
+#define MCP23017_IODIRA_BANK1       0x00    /*I/O DIRECTION REGISTER A*/
+#define MCP23017_IODIRB_BANK1       0x10    /*I/O DIRECTION REGISTER B*/
+#define MCP23017_IPOLA_BANK1        0x01    /*INPUT POLARITY REGISTER A*/
+#define MCP23017_IPOLB_BANK1        0x11    /*INPUT POLARITY REGISTER B*/
+#define MCP23017_GPINTENA_BANK1     0x02    /*INTERRUPT-ON-CHANGE CONTROL REGISTER A*/
+#define MCP23017_GPINTENB_BANK1     0x12    /*INTERRUPT-ON-CHANGE CONTROL REGISTER B*/
+#define MCP23017_DEFVALA_BANK1      0x03    /*DEFAULT COMPARE REGISTER FOR INTERRUPT-ON-CHANGE A*/
+#define MCP23017_DEFVALB_BANK1      0x13    /*DEFAULT COMPARE REGISTER FOR INTERRUPT-ON-CHANGE B*/
+#define MCP23017_INTCONA_BANK1      0x04    /*INTERRUPT CONTROL REGISTER A*/
+#define MCP23017_INTCONB_BANK1      0x14    /*INTERRUPT CONTROL REGISTER B*/
+#define MCP23017_IOCONA_BANK1       0x05    /*CONFIGURATION REGISTER A*/
+#define MCP23017_IOCONB_BANK1       0x15    /*CONFIGURATION REGISTER B*/
+#define MCP23017_GPPUA_BANK1        0x06    /*PULL-UP RESISTOR A*/
+#define MCP23017_GPPUB_BANK1        0x16    /*PULL-UP RESISTOR B*/
+#define MCP23017_INTFA_BANK1        0x07    /*INTERRUPT FLAG REGISTER A*/
+#define MCP23017_INTFB_BANK1        0x17    /*INTERRUPT FLAG REGISTER B*/
+#define MCP23017_INTCAPA_BANK1      0x08    /*INTERRUPT CAPTURE REGISTER A*/
+#define MCP23017_INTCAPB_BANK1      0x18    /*INTERRUPT CAPTURE REGISTER B*/
+#define MCP23017_GPIOA_BANK1        0x09    /*PORT REGISTER A*/
+#define MCP23017_GPIOB_BANK1        0x19    /*PORT REGISTER B*/
+#define MCP23017_OLATA_BANK1        0x0A    /*OUTPUT LATCH REGISTER A*/
+#define MCP23017_OLATB_BANK1        0x1A    /*OUTPUT LATCH REGISTER B*/
+
+#define MCP23017_IOCON_BANK1        MCP23017_IOCONA_BANK
+
+/* IOCON bits */
+#define MCP23017_INTPOL             0x20    /* This bit sets the polarity of the INT output pin.*/
+#define MCP23017_ODR                0x40    /* This bit configures the INT pin as an open-drain output.*/
+#define MCP23017_HAEN               0x80    /* Hardware Address Enable bit (MCP23S17 only). Address pins are always enabled on MCP23017.*/
+#define MCP23017_DISSLW             0x10    /* Slew Rate control bit for SDA output.*/
+#define MCP23017_SEQOP              0x20    /* Sequential Operation mode bit.*/
+#define MCP23017_MIRROR             0x40    /* INT Pins Mirror bit.*/
+#define MCP23017_BANK               0x80    /* Controls how the registers are addressed.*/
+
+/* All other registers effect the associated pin. The following can be used to address these. */
+#define MCP23017_PIN0               0x01
+#define MCP23017_PIN1               0x02
+#define MCP23017_PIN2               0x04
+#define MCP23017_PIN3               0x08
+#define MCP23017_PIN4               0x10
+#define MCP23017_PIN5               0x20
+#define MCP23017_PIN6               0x40
+#define MCP23017_PIN7               0x80
 
 
 
